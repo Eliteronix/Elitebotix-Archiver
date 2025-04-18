ab6579860e920ea0ce307781f07992821ac7d4d7
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-19 01:38:45 +0200

    Added initial files

---
+++ index.js
@@ -0,0 +1,37 @@
+//Log message upon starting the bot
+// eslint-disable-next-line no-console
+console.log('Bot is starting...');
+
+require('dotenv').config();
+
+//require the discord.js module
+const Discord = require('discord.js');
+
+//create a Discord client with discord.js
+const client = new Discord.Client({
+	partials: [
+		Discord.Partials.Channel,
+	]
+});
+
+//login with the Discord client using the Token from the .env file
+// eslint-disable-next-line no-undef
+client.login(process.env.BOTTOKEN);
+
+//declare what the discord client should do when it's ready
+client.on('ready', readyDiscord);
+
+//declare the function which will be used when ready
+async function readyDiscord() {
+	//log a message when ready
+	// eslint-disable-next-line no-console
+	console.log('The Bot is ready.');
+
+	client.user.setPresence({
+		status: 'online',  //You can show online, idle....
+		activities: [{
+			name: 'with Elitebotix',  //The message shown
+			// type: 'PLAYING' //PLAYING: WATCHING: LISTENING: STREAMING:
+		}]
+	});
+}
