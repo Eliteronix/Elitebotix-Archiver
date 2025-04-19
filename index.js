5d3e63ec7b300efe3c52a7957882d9f6b062070e
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-19 02:25:54 +0200

    Added more structure

--- index.js
+++ index.js
@@ -3,3 +3,19 @@
 console.log('Bot is starting...');
 
 require('dotenv').config();
+
+const { scrape } = require('./scrape.js');
+
+scrapeForNewMatches();
+
+async function scrapeForNewMatches() {
+	try {
+		await scrape.execute();
+	} catch (e) {
+		console.error('index.js | scrape' + e);
+	}
+
+	setTimeout(() => {
+		scrapeForNewMatches();
+	}, 650);
+}
--- pm2.config.js
+++ pm2.config.js
@@ -6,18 +6,7 @@ module.exports = {
 	interpreter: "bun", // Bun interpreter
 	watch: true, // Watch for file changes
 	ignore_watch: [
-		"databases",
-		".git",
-		"maps",
-		"package-lock.json",
-		"matchLogs",
-		"node_modules",
-		"listcovers",
-		"beatmapcovers",
-		"badges",
-		"slimcovers",
-		"avatars",
-		"wrappedcards",
+		"lastImport.json",
 	],
 	env: {
 		PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
