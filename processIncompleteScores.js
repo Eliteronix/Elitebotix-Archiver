ca39a1a4c19644f3dbd318e9fde6bb2768ffe6bc
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-19 19:35:39 +0200

    Update processIncompleteScores.js

--- processIncompleteScores.js
+++ processIncompleteScores.js
@@ -1,5 +1,8 @@
 module.exports = {
 	async processIncompleteScores() {
 		console.log('processIncompleteScores.js | processIncompleteScores');
+		//Return after 1 minutes
+		await new Promise(resolve => setTimeout(resolve, 1 * 60 * 1000));
+		return;
 	}
 };
