12ef1a142e3ddb2ac74f1a8999acef02e5ea1dc3
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-20 17:42:51 +0200

    Moved over some new code

--- dbObjects.js
+++ dbObjects.js
@@ -31,6 +31,21 @@ const elitebotixMultiGameScores = new Sequelize('database', 'username', 'passwor
 	}
 });
 
+const elitebotixMultiGames = new Sequelize('database', 'username', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: `${process.env.ELITEBOTIXROOTPATH}/databases/multiGames.sqlite`,
+	retry: {
+		max: 25, // Maximum retry 15 times
+		backoffBase: 100, // Initial backoff duration in ms. Default: 100,
+		backoffExponent: 1.14, // Exponent to increase backoff each try. Default: 1.1
+	},
+	pool: {
+		max: 7,
+	}
+});
+
 const elitebotixMultiMatches = new Sequelize('database', 'username', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
@@ -48,10 +63,12 @@ const elitebotixMultiMatches = new Sequelize('database', 'username', 'password',
 
 const DBElitebotixProcessQueue = require(`${process.env.ELITEBOTIXROOTPATH}/models/DBProcessQueue`)(elitebotixProcessQueue, Sequelize.DataTypes);
 const DBElitebotixOsuMultiMatches = require(`${process.env.ELITEBOTIXROOTPATH}/models/DBOsuMultiMatches`)(elitebotixMultiMatches, Sequelize.DataTypes);
+const DBElitebotixOsuMultiGames = require(`${process.env.ELITEBOTIXROOTPATH}/models/DBOsuMultiGames`)(elitebotixMultiGames, Sequelize.DataTypes);
 const DBElitebotixOsuMultiGameScores = require(`${process.env.ELITEBOTIXROOTPATH}/models/DBOsuMultiGameScores`)(elitebotixMultiGameScores, Sequelize.DataTypes);
 
 module.exports = {
 	DBElitebotixProcessQueue,
 	DBElitebotixOsuMultiMatches,
+	DBElitebotixOsuMultiGames,
 	DBElitebotixOsuMultiGameScores,
 };
--- processIncompleteScores.js
+++ processIncompleteScores.js
@@ -1,6 +1,90 @@
+const { DBElitebotixOsuMultiGames } = require("./dbObjects");
+const osu = require('node-osu');
+
 module.exports = {
 	async processIncompleteScores() {
-		console.log('processIncompleteScores.js | processIncompleteScores');
+		let incompleteMatchScore = await DBElitebotixOsuMultiGames.findOne({
+			attributes: ['id', 'matchId', 'updatedAt'],
+			where: {
+				tourneyMatch: true,
+				warmup: null
+			},
+			order: [
+				['updatedAt', 'ASC']
+			]
+		});
+
+		console.log('incompleteMatchScore', incompleteMatchScore);
+
+		if (incompleteMatchScore) {
+			const osuApi = new osu.Api(APItoken, {
+				// baseUrl: sets the base api url (default: https://osu.ppy.sh/api)
+				notFoundAsError: true, // Throw an error on not found instead of returning nothing. (default: true)
+				completeScores: false, // When fetching scores also fetch the beatmap they are for (Allows getting accuracy) (default: false)
+				parseNumeric: false // Parse numeric values into numbers/floats, excluding ids
+			});
+
+			await osuApi.getMatch({ mp: incompleteMatchScore.matchId })
+				.then(async (match) => {
+					if (logBroadcastEval) {
+						// eslint-disable-next-line no-console
+						console.log('Broadcasting processQueueTasks/saveMultiMatches.js incompleteMatchScore to shards...');
+					}
+
+					client.shard.broadcastEval(async (c, { channelId, message }) => {
+						let channel = await c.channels.cache.get(channelId);
+						if (channel) {
+							await channel.send(message);
+						}
+					}, { context: { channelId: channelId, message: `<https://osu.ppy.sh/mp/${match.id}> | ${incompleteMatchScore.updatedAt.getUTCHours().toString().padStart(2, 0)}:${incompleteMatchScore.updatedAt.getUTCMinutes().toString().padStart(2, 0)} ${incompleteMatchScore.updatedAt.getUTCDate().toString().padStart(2, 0)}.${(incompleteMatchScore.updatedAt.getUTCMonth() + 1).toString().padStart(2, 0)}.${incompleteMatchScore.updatedAt.getUTCFullYear()} | \`${match.name}\`` } });
+
+					incompleteMatchScore.changed('updatedAt', true);
+					await incompleteMatchScore.save();
+
+					await saveOsuMultiScores(match, client);
+				})
+				.catch(async (err) => {
+					logDatabaseQueries(2, 'saveOsuMultiScores.js DBOsuMultiGames incomplete scores backup');
+					let incompleteGames = await DBOsuMultiGames.findAll({
+						attributes: ['id', 'warmup', 'updatedAt'],
+						where: {
+							matchId: incompleteMatchScore.matchId
+						}
+					});
+
+					logDatabaseQueries(2, 'saveOsuMultiScores.js DBOsuMultiGameScores incomplete scores backup');
+					let incompleteScores = await DBOsuMultiGameScores.findAll({
+						attributes: ['id', 'maxCombo', 'pp', 'updatedAt'],
+						where: {
+							matchId: incompleteMatchScore.matchId
+						}
+					});
+
+					if (err.message === 'Not found') {
+						for (let i = 0; i < incompleteGames.length; i++) {
+							incompleteGames[i].warmup = false;
+							await incompleteGames[i].save();
+						}
+
+						for (let i = 0; i < incompleteScores.length; i++) {
+							incompleteScores[i].maxCombo = 0;
+							incompleteScores[i].pp = 0;
+							await incompleteScores[i].save();
+						}
+					} else {
+						for (let i = 0; i < incompleteGames.length; i++) {
+							incompleteGames[i].changed('updatedAt', true);
+							await incompleteGames[i].save();
+						}
+
+						for (let i = 0; i < incompleteScores.length; i++) {
+							incompleteScores[i].changed('updatedAt', true);
+							await incompleteScores[i].save();
+						}
+					}
+				});
+		}
+
 		//Return after 1 minutes
 		await new Promise(resolve => setTimeout(resolve, 1 * 60 * 1000));
 		return;
