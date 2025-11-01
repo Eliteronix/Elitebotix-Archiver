048647e48c0b3f3fd19b53dc8fcb1c6f8f040418
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2025-11-01 18:34:52 +0000

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 module.exports = {
 	name: 'Elitebotix Archiver', // Name of your application
 	script: 'index.js', // Entry point of your application
-	interpreter: 'bun', // Bun interpreter
+	//interpreter: 'bun', // Bun interpreter
 	watch: returnBoolean(process.env.SERVER), // Watch for file changes
 	ignore_watch: [
 		'lastImport.json',
 		'.git'
 	],
-	env: {
-		PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-	}
+	//env: {
+	//	PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
+	//}
 };
 
 function returnBoolean(value) {
