94caa98171d16cf75de7af74ac774a29431c1f91
Eliteronix <zimmermann.mariomarvin@gmail.com>
2026-05-11 19:34:50 +0200

    Added promise.race to fix hanging

--- scrape.js
+++ scrape.js
@@ -62,132 +62,141 @@ module.exports = {
 		// console.log('Using API token index:', parseInt(lastImport.matchId) % process.env.OSUTOKENSV1.split('-').length);
 
 		osuApiRequests.inc();
-		console.log('Fetching match with ID:', lastImport.matchId);
-		await osuApi.getMatch({ mp: lastImport.matchId })
-			.then(async (match) => {
-				console.log('Match found:', match.name, 'with ID:', match.id);
-				lastImport.lastMatchFound = lastImport.matchId;
+		// console.log('Fetching match with ID:', lastImport.matchId);
+		await Promise.race([
+			new Promise(resolve => setTimeout(() => resolve('timeout'), 10000)),
+			osuApi.getMatch({ mp: lastImport.matchId })
+				.then(async (match) => {
+					// console.log('Match found:', match.name, 'with ID:', match.id);
+					lastImport.lastMatchFound = lastImport.matchId;
 
-				timeBehindMatchCreation.set(Math.floor((Date.now() - Date.parse(match.raw_start)) / 1000));
+					timeBehindMatchCreation.set(Math.floor((Date.now() - Date.parse(match.raw_start)) / 1000));
 
-				let sixHoursAgo = new Date();
-				sixHoursAgo.setUTCHours(sixHoursAgo.getUTCHours() - 6);
+					let sixHoursAgo = new Date();
+					sixHoursAgo.setUTCHours(sixHoursAgo.getUTCHours() - 6);
 
-				let fiveMinutesAgo = new Date();
-				fiveMinutesAgo.setUTCMinutes(fiveMinutesAgo.getUTCMinutes() - 5);
-				if (match.raw_end || Date.parse(match.raw_start) < sixHoursAgo) {
-					if (match.name.toLowerCase().match(/.+:.+vs.+/g)) {
-						// console.log('Match ended or started over 6 hours ago, saving scores and importing match:', match.name);
-						await saveOsuMultiScores(match);
-						// console.log('Match saved, adding import task to queue for match:', match.name);
-						let now = new Date();
-						let minutesBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60) % 60;
-						let hoursBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60 / 60) % 24;
-						let daysBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60 / 60 / 24);
+					let fiveMinutesAgo = new Date();
+					fiveMinutesAgo.setUTCMinutes(fiveMinutesAgo.getUTCMinutes() - 5);
+					if (match.raw_end || Date.parse(match.raw_start) < sixHoursAgo) {
+						if (match.name.toLowerCase().match(/.+:.+vs.+/g)) {
+							// console.log('Match ended or started over 6 hours ago, saving scores and importing match:', match.name);
+							await saveOsuMultiScores(match);
+							// console.log('Match saved, adding import task to queue for match:', match.name);
+							let now = new Date();
+							let minutesBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60) % 60;
+							let hoursBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60 / 60) % 24;
+							let daysBehindToday = parseInt((now.getTime() - Date.parse(match.raw_start)) / 1000 / 60 / 60 / 24);
 
-						await DBElitebotixProcessQueue.create({
-							guildId: 'None',
-							task: 'messageChannel',
-							additions: `${process.env.IMPORTMATCHLOG};<https://osu.ppy.sh/mp/${lastImport.matchId}> ${daysBehindToday}d ${hoursBehindToday}h ${minutesBehindToday}m \`${match.name}\` done`,
-							priority: 1,
-							date: new Date()
-						});
-						// console.log('Import task added to queue for match:', match.name);
-					}
-
-					//Go next if match found and ended / too long going already
-					lastImport.matchId = lastImport.matchId + 1;
-
-					//Create the lastImport.json file
-					fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
-					// console.log('Match ended or started over 6 hours ago, going next match:', lastImport.matchId);
-					return;
-				} else if (Date.parse(match.raw_start) < fiveMinutesAgo) {
-					if (match.name.toLowerCase().match(/.+:.+vs.+/g)) {
-						// console.log('Match started over 5 minutes ago, saving scores and importing match:', match.name);
-						await saveOsuMultiScores(match);
-						let date = new Date();
-						date.setUTCMinutes(date.getUTCMinutes() + 5);
-
-						// console.log('Match saved, adding import task to queue for match:', match.name, 'with execution time of:', date);
-						await DBElitebotixProcessQueue.create({
-							guildId: 'None',
-							task: 'importMatch',
-							additions: `${lastImport.matchId};1;${Date.parse(match.raw_start)};${match.name}`,
-							priority: 1,
-							date: date
-						});
-						// console.log('Import task added to queue for match:', match.name, 'with execution time of:', date);
-
-						await DBElitebotixProcessQueue.create({
-							guildId: 'none',
-							task: 'updateCurrentMatches',
-							date: new Date(),
-							priority: 0
-						});
-						// console.log('Update current matches task added to queue for match:', match.name);
-					}
-
-					//Go next if match found and ended / too long going already
-					lastImport.matchId = lastImport.matchId + 1;
-
-					//Create the lastImport.json file
-					fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
-					// console.log('Match started over 5 minutes ago, going next match:', lastImport.matchId);
-					return;
-				}
-
-				// console.log('Match found but not ended, checking for incomplete scores and saving match if needed:', match.name);
-				return await processIncompleteScores();
-			})
-			.catch(async (err) => {
-				console.log('Error fetching match:', err.message, `for match ID: ${lastImport.matchId}`);
-				if (err.message === 'Not found') {
-					//Fallback in case we got ahead of the matches
-					if (lastImport.lastMatchFound < lastImport.matchId - 100) {
-						lastImport.matchId = lastImport.lastMatchFound;
-						// eslint-disable-next-line no-console
-						console.log('Match not found for 100 matches in a row, going back to last match found:', lastImport.matchId);
-					} else {
-						//Go next if match not found
-						lastImport.matchId = lastImport.matchId + 1;
-					}
-
-					// console.log('Match not found, going next match:', lastImport.matchId);
-					//Create the lastImport.json file
-					fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
-					// console.log('Match not found, going next match; written into JSON:', lastImport.matchId);
-					return;
-				} else {
-					// console.log('Error fetching match, trying to check if match is over 24 hours long or if there is an API issue:', err.message, `for match ID: ${lastImport.matchId}`);
-					try {
-						// Check using node fetch
-						//const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-						osuWebRequests.inc();
-						let response = await fetch(`https://osu.ppy.sh/community/matches/${parseInt(lastImport.matchId)}`);
-						let htmlCode = await response.text();
-						let isolatedContent = htmlCode.replace(/[\s\S]+<script id="json-events" type="application\/json">/gm, '').replace(/<\/script>[\s\S]+/gm, '');
-						let json = JSON.parse(isolatedContent);
-						if (Date.parse(json.events[json.events.length - 1].timestamp) - Date.parse(json.match.start_time) > 86400000) {
-							//Go next if over 24 hours long game
-							lastImport.matchId = lastImport.matchId + 1;
-
-							//Create the lastImport.json file
-							// console.log('Match over 24 hours long, going next match:', lastImport.matchId);
-							fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
-							// console.log('Match over 24 hours long, going next match; written into JSON:', lastImport.matchId);
-							return;
-						} else {
-							return;
+							await DBElitebotixProcessQueue.create({
+								guildId: 'None',
+								task: 'messageChannel',
+								additions: `${process.env.IMPORTMATCHLOG};<https://osu.ppy.sh/mp/${lastImport.matchId}> ${daysBehindToday}d ${hoursBehindToday}h ${minutesBehindToday}m \`${match.name}\` done`,
+								priority: 1,
+								date: new Date()
+							});
+							// console.log('Import task added to queue for match:', match.name);
 						}
-					} catch (error) {
-						console.error(error, `API Key Index ${parseInt(lastImport.matchId) % process.env.OSUTOKENSV1.split('-').length} going same saveMultiMatches.js https://osu.ppy.sh/community/matches/${parseInt(lastImport.matchId)}`);
-						//Go same if error
-						//Return after 5 minutes
-						await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000));
+
+						//Go next if match found and ended / too long going already
+						lastImport.matchId = lastImport.matchId + 1;
+
+						//Create the lastImport.json file
+						fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
+						// console.log('Match ended or started over 6 hours ago, going next match:', lastImport.matchId);
+						return;
+					} else if (Date.parse(match.raw_start) < fiveMinutesAgo) {
+						if (match.name.toLowerCase().match(/.+:.+vs.+/g)) {
+							// console.log('Match started over 5 minutes ago, saving scores and importing match:', match.name);
+							await saveOsuMultiScores(match);
+							let date = new Date();
+							date.setUTCMinutes(date.getUTCMinutes() + 5);
+
+							// console.log('Match saved, adding import task to queue for match:', match.name, 'with execution time of:', date);
+							await DBElitebotixProcessQueue.create({
+								guildId: 'None',
+								task: 'importMatch',
+								additions: `${lastImport.matchId};1;${Date.parse(match.raw_start)};${match.name}`,
+								priority: 1,
+								date: date
+							});
+							// console.log('Import task added to queue for match:', match.name, 'with execution time of:', date);
+
+							await DBElitebotixProcessQueue.create({
+								guildId: 'none',
+								task: 'updateCurrentMatches',
+								date: new Date(),
+								priority: 0
+							});
+							// console.log('Update current matches task added to queue for match:', match.name);
+						}
+
+						//Go next if match found and ended / too long going already
+						lastImport.matchId = lastImport.matchId + 1;
+
+						//Create the lastImport.json file
+						fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
+						// console.log('Match started over 5 minutes ago, going next match:', lastImport.matchId);
 						return;
 					}
-				}
-			});
+
+					// console.log('Match found but not ended, checking for incomplete scores and saving match if needed:', match.name);
+					return await processIncompleteScores();
+				})
+				.catch(async (err) => {
+					// console.log('Error fetching match:', err.message, `for match ID: ${lastImport.matchId}`);
+					if (err.message === 'Not found') {
+						//Fallback in case we got ahead of the matches
+						if (lastImport.lastMatchFound < lastImport.matchId - 100) {
+							lastImport.matchId = lastImport.lastMatchFound;
+							// eslint-disable-next-line no-console
+							console.log('Match not found for 100 matches in a row, going back to last match found:', lastImport.matchId);
+						} else {
+							//Go next if match not found
+							lastImport.matchId = lastImport.matchId + 1;
+						}
+
+						// console.log('Match not found, going next match:', lastImport.matchId);
+						//Create the lastImport.json file
+						fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
+						// console.log('Match not found, going next match; written into JSON:', lastImport.matchId);
+						return;
+					} else {
+						// console.log('Error fetching match, trying to check if match is over 24 hours long or if there is an API issue:', err.message, `for match ID: ${lastImport.matchId}`);
+						try {
+							// Check using node fetch
+							//const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+							osuWebRequests.inc();
+							let response = await fetch(`https://osu.ppy.sh/community/matches/${parseInt(lastImport.matchId)}`);
+							let htmlCode = await response.text();
+							let isolatedContent = htmlCode.replace(/[\s\S]+<script id="json-events" type="application\/json">/gm, '').replace(/<\/script>[\s\S]+/gm, '');
+							let json = JSON.parse(isolatedContent);
+							if (Date.parse(json.events[json.events.length - 1].timestamp) - Date.parse(json.match.start_time) > 86400000) {
+								//Go next if over 24 hours long game
+								lastImport.matchId = lastImport.matchId + 1;
+
+								//Create the lastImport.json file
+								// console.log('Match over 24 hours long, going next match:', lastImport.matchId);
+								fs.writeFileSync('./lastImport.json', JSON.stringify(lastImport, null, 2), 'utf-8');
+								// console.log('Match over 24 hours long, going next match; written into JSON:', lastImport.matchId);
+								return;
+							} else {
+								return;
+							}
+						} catch (error) {
+							console.error(error, `API Key Index ${parseInt(lastImport.matchId) % process.env.OSUTOKENSV1.split('-').length} going same saveMultiMatches.js https://osu.ppy.sh/community/matches/${parseInt(lastImport.matchId)}`);
+							//Go same if error
+							//Return after 5 minutes
+							await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000));
+							return;
+						}
+					}
+				})
+		]).then(async (result) => {
+			if (result === 'timeout') {
+				// eslint-disable-next-line no-console
+				console.log('API call timed out for match ID:', lastImport.matchId);
+				await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000));
+			}
+		});
 	},
 };
